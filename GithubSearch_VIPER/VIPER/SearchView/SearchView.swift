067eca83865083cee98321e683dc583e15720242
Arachi <kbw2204@naver.com>
2021-03-27 23:29:37 +0900

    [improvement] gitHubService 작성 (#2)

--- GithubSearch_VIPER/VIPER/SearchView/SearchView.swift
+++ GithubSearch_VIPER/VIPER/SearchView/SearchView.swift
@@ -9,14 +9,24 @@ import UIKit
 
 import SnapKit
 import Then
+import RxSwift
 
 final class SearchView: UIViewController {
   var presenter: SearchPresenterProtocol?
+  let disposeBag = DisposeBag()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    
+    // 임시
+    GithubService()
+      .searchRepos(with: "rxSwift")
+      .compactMap { $0 }
+      .subscribe(onNext: { repos in
+        for repo in repos {
+          print(repo.fullName)
+        }
+      }).disposed(by: self.disposeBag)
   }
 }
 
